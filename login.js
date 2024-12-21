af80de5ca59fe147d363e835ee8a6227158f5739
jai adhithya <jaiadhithya.k2024aids@sece.ac.in>
2024-12-21 20:26:00 +0530

    second jai commit

---
+++ login.js
@@ -0,0 +1,7 @@
+const name=document.getElementById('user')
+const pass=document.getElementById('pass')
+const form=document.getElementById('form')
+form.addEventListener('submit',(e)=>
+{
+    e.preventDefault()
+})
