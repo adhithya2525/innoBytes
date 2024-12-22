d561458759591c2d91c5f0b8f58b43df0177be6f
jai adhithya <jaiadhithya.k2024aids@sece.ac.in>
2024-12-22 12:16:17 +0530

    Please enter the commit message for your changes. Lines starting jai commit 3

--- login.js
+++
@@ -1,7 +0,0 @@
-const name=document.getElementById('user')
-const pass=document.getElementById('pass')
-const form=document.getElementById('form')
-form.addEventListener('submit',(e)=>
-{
-    e.preventDefault()
-})
